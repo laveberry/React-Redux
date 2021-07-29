89c205532452518289458e6290e5e1254a638941
dahye <laveberry7@gmail.com>
2021-07-29 18:53:00 +0900

    redux 시작

---
+++ src/components/DisplayNumber.jsx
@@ -0,0 +1,25 @@
+import React, {Componet} from 'react';
+import store from "../store";
+
+export default class DisplayNumber extends Componet {
+    //내부 데이터 생성
+    state = {number:store.getState(), number}
+    //재호출을 위한 생성자
+    constructor(props){
+        super(props);
+        store.subscribe(function(){
+            this.setState({number:store.getState().number});
+        }.bind(this));
+    }
+    render(){
+        return (
+            <div>
+                <h1>Display Number</h1>
+                {/* <input type="text" value={this.props.number} readOnly/> */}
+                <input type="text" value={this.state.number} readOnly/>
+            </div>
+        )
+            
+        
+    }
+}
---
+++ src/store.js
@@ -0,0 +1,19 @@
+import {createStore} from 'redux';
+
+// 리듀서 : 두개의 인자 받음 state(데이터), action(가해지는행위)
+// redux devtool 설정
+export default createStore(function(state, action){
+    
+    //if문 설정
+    if(state=== undefined){
+        return {number:0}
+    }
+    if(action.type === 'INCREMENT'){
+        //...값 : state 복제, 추가되는 프로퍼티
+        return {...state, number:state.number + action.size}
+    }
+
+    return state;
+
+}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
